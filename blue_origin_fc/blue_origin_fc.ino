77aaba99d3e28650ad35aa692dc83c9d28e88d9f
Eli Reed <sailedeer11@gmail.com>
2020-01-16 18:03:31 -0800

    Redefined possible lab states

--- blue_origin_fc/blue_origin_fc.ino
+++ blue_origin_fc/blue_origin_fc.ino
@@ -37,10 +37,10 @@
 #define LS_NO_STATE 0
 #define LS_IDLE 1
 #define LS_SERIAL_READ 2
-#define LS_PUMP_FILL 3
-#define LS_PUMP_EMPTY 4
-#define LS_CELL_PLATING 5
-#define LS_LOGGING 6
+#define LS_PRIME_EXPERIMENT 3
+#define LS_CELL_PLATING 4
+#define LS_CLEAN_CELL 5
+#define LS_LOG 6
 
 // file names for logging, keeping track of state, etc.
 #define LOG_FILE "log.txt"
@@ -181,49 +181,58 @@ void loop() {
     case LS_IDLE:
       {     
         if (Serial.available() > 0) {
-          last_lab_state = LS_IDLE;
-          lab_state = LS_SERIAL_READ;
-          break;
+          read_serial_input();
         }
-        unsigned long t = millis();
-        if (t - last_log_time >= 1000) {
-          last_log_time = t;
-          last_lab_state = LS_IDLE;
-          lab_state = LS_LOGGING;
-          break;
-        }
-        if (blue_state.equals(BS_COAST_START)) {
-          last_lab_state = LS_IDLE;
-          lab_state = LS_CELL_PLATING;
-          break;
-        }
-        if (blue_state.equals(BS_NO_STATE)) {
-          // is it time for us to start priming the experiment?
-          // if not, that me
-          if (!experiment_primed) {
-            // enter priming state
-          }
-        }
-        if (blue_state.equals(BS_LANDING)) {
-          if (pump_empty) {}
-        }
-        last_lab_state = LS_IDLE;
-        lab_state = LS_IDLE;
       }
       break;
     case LS_SERIAL_READ:
       {
-        File lf = SD.open(LOG_FILE, FILE_WRITE);
-        delay(20);
-        for (int i = 0; i < 21; i++) {
-          char buf[21];
-          int bytes = 0;
-          char next = Serial.read();
-          while (next > -1 && bytes < 21 && next != ',') {
-            buf[bytes] = next;
-            bytes++;
-            next = Serial.read();
-          }
+        
+      }
+      break;
+      
+    case LS_PRIME_EXPERIMENT:
+      {
+
+      }
+      break;
+      
+    case LS_CELL_PLATING:
+      {
+        
+      }
+      break;
+
+    case LS_CLEAN_CELL:
+      {
+        
+      }
+      break;
+      
+    case LS_LOG:
+      { 
+        
+      }
+      break;
+    default:
+      {
+        
+      }
+      break;
+  }
+}
+
+void read_serial_input() {
+  delay(20);
+  for (int i = 0; i < 21; i++) {
+    char buf[21];
+    int bytes = 0;
+    char next = Serial.read();
+    while (next > -1 && bytes < 21 && next != ',') {
+      buf[bytes] = next;
+      bytes++;
+      next = Serial.read();
+    }
 //          for (bytes = 0; bytes <= 21; bytes++) {
 //            if (Serial.peek() > -1) {
 //              char next = Serial.read();
@@ -236,92 +245,17 @@ void loop() {
 //              break;
 //            }
 //          }
-          buf[bytes] = '\0';
-          if (i == 0) {
-            blue_state = buf;
-          }
-          if (i == 1) {
-            last_blue_time = ((String)buf).toFloat();
-          }
-        }
-        lab_state = last_lab_state;
-        last_lab_state = LS_SERIAL_READ;
-        lf.close();
-      }
-      break;
-      
-    case LS_PUMP_FILL:
-      {
-        // set pump power high
-        lab_state = last_lab_state;
-        digitalWrite(PUMP_1, HIGH);
-        last_lab_state = LS_PUMP_FILL;
-      }
-      break;
-      
-    case LS_PUMP_EMPTY:
-      {
-        lab_state = last_lab_state;
-        digitalWrite(PUMP_2, HIGH);
-        last_lab_state = LS_PUMP_EMPTY;
-      }
-      break;
-      
-    case LS_CELL_PLATING:
-      {
-        File lf = SD.open(LOG_FILE, FILE_WRITE);
-        if (blue_state.equals(BS_COAST_START) || blue_state.equals(BS_APOGEE)) {
-          if (!plating_started) {
-            // start plating
-            digitalWrite(PUMP_POWER, LOW);
-            digitalWrite(EXPERIMENT, HIGH);
-            plating_started = true;
-            lf.close();
-          }
-          if (Serial.available() > 0) {
-            last_lab_state = LS_CELL_PLATING;
-            lab_state = LS_SERIAL_READ;
-            break;
-          }
-          unsigned long t = millis();
-          if (t - last_log_time >= 1000) {
-            last_log_time = t;
-            last_lab_state = LS_CELL_PLATING;
-            lab_state = LS_LOGGING;
-            break;
-          }
-        } else {
-          digitalWrite(EXPERIMENT, LOW);
-          File lf = SD.open(LOG_FILE, FILE_WRITE);
-          log_event("Plating ended", lf);
-          lf.close();          
-          lab_state = LS_IDLE;
-          last_lab_state = LS_CELL_PLATING;
-        }
-      }
-      break;
-      
-    case LS_LOGGING:
-      { 
-        float curr_val = analogRead(CURR_ANALOG_PIN) * (5.0 / 1023.0);
-        float volt_val = 5.0 - analogRead(VOLT_ANALOG_PIN) * (5.0 / 1023.0);
-        float temp_val = analogRead(TEMP_ANALOG_PIN) * (5.0 / 1023.0) * 0.01;
-        
-        log_event("Blue is in state: " + blue_state + " after " + String(last_blue_time) + " seconds.", lf);
-        lab_state = last_lab_state;
-        last_lab_state = LS_LOGGING;
-      }
-      break;
-    default:
-      {
-        File lf = SD.open(LOG_FILE, FILE_WRITE);
-        log_event("Reached a null state. Last lab state: " + String(last_lab_state), lf);
-        lab_state = LS_IDLE;
-        last_lab_state = LS_NO_STATE;
-        lf.close();
-      }
-      break;
+    buf[bytes] = '\0';
+    if (i == 0) {
+      blue_state = buf;
+    }
+    if (i == 1) {
+      last_blue_time = ((String)buf).toFloat();
+    }
   }
+  lab_state = last_lab_state;
+  last_lab_state = LS_SERIAL_READ;
+  lf.close();
 }
 
 String get_time_stamp() {
