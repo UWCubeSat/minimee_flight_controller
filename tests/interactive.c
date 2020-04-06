ad8ae5f61f5771e3c0397ec06a2437a8ff09291c
Mark Polyakov <mark@markasoftware.com>
2020-04-06 00:36:18 -0700

    show pin status.Probably should alloc our own IRQs so that we can use filtering, and maybe even inversion (which would avoid the struct)

--- tests/interactive.c
+++ tests/interactive.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <signal.h>
 #include <string.h>
 #include <errno.h>
@@ -20,18 +21,54 @@
 sd_t sd;
 avr_t *avr;
 
-char buffer[256];
-unsigned char buffer_idx = 0;
+typedef struct arduino_pin {
+	bool active_high;
+	char *name;
+} arduino_pin;
+
+static void pin_change_hook(avr_irq_t *irq, uint32_t value, void *param) {
+	arduino_pin *a = (arduino_pin*)param;
+	char *desc = value == a->active_high ? "ON" : "OFF";
+	printf("%s turned %s\n", a->name, desc);
+}
 
 static void sigint_handler(int sig) {
 	printf("> ");
+	char buffer[1024];
 	scanf("%s", buffer);
-	buffer_idx = 0;
+	if (strlen(buffer) > 64) {
+		puts("String longer than serial buffer not supported yet.");
+		return;
+	}
 	if (strcmp(buffer, "quit") == 0) {
 		fputs("Saving SD card to disk...", stderr);
 		sd_free(&sd);
 		fputs("Exiting cleanly!", stderr);
-		exit(0);
+		return exit(0);
+	}
+
+	double voltage;
+	if (sscanf(buffer, "voltage=%lf", &voltage)) {
+		// TODO: set voltage
+		printf("TODO: Set voltage to %f\n", voltage);
+		return;
+	}
+
+	double current;
+	if (sscanf(buffer, "current=%lf", &current)) {
+		printf("TODO: Set current to %f\n", current);
+	}
+
+	int temperature;
+	if (sscanf(buffer, "temperature=%d", &temperature)) {
+		printf("TODO: Set temperature to %d\n", temperature);
+	}
+
+	for (int i = 0; buffer[i] != '\0'; i++) {
+		avr_raise_irq(avr_io_getirq(avr,
+					    AVR_IOCTL_UART_GETIRQ('0'),
+					    UART_IRQ_INPUT),
+			      buffer[i]);
 	}
 }
 
@@ -78,25 +115,33 @@ int main(int argc, char **argv) {
 		avr_io_getirq(avr, AVR_IOCTL_IOPORT_GETIRQ('C'), 0),
 		sd.irq + SD_IRQ_CS);
 
-	/////// INITIALIZE UART_PTY
-	/* uart_pty_init(avr, &uart_pty); */
-	/* uart_pty_connect(&uart_pty, '0'); */
+	/////// PIN IRQS
+#define WATCH_PIN(port, pin, _name, _active_high)			\
+	arduino_pin _name = {						\
+		.name = #_name,						\
+		.active_high = _active_high,				\
+	};								\
+	avr_irq_register_notify(					\
+		avr_io_getirq(avr, AVR_IOCTL_IOPORT_GETIRQ(port), pin),	\
+		pin_change_hook, &_name)
+
+	WATCH_PIN('D', 2, PUMP_POWER, false);
+	WATCH_PIN('D', 5, PUMP_1, true);
+	WATCH_PIN('D', 6, PUMP_2, true);
+	WATCH_PIN('B', 0, SOL_1, true);
+	WATCH_PIN('B', 1, SOL_2, true);
+	WATCH_PIN('B', 2, SOL_3, true);
+	WATCH_PIN('D', 3, MOTOR, true);
+	WATCH_PIN('C', 5, EXPERIMENT, false);
 
 	/////// CATCH SIGNALS
 	sigaction(SIGINT, &sigint_action, NULL);
 	sigaction(SIGTERM, &sigint_action, NULL);
 
 	/////// RUN SIM
-	puts("About to start");
-	// give time for xterm and picocom to start
-	sleep(1);
 	unsigned long cycles_run = 0;
-	buffer[0] = '\0';
+	puts("About to start");
 	while (1) {
-		if (buffer[buffer_idx] != '\0' && cycles_run % 1000000 == 0) {
-			avr_raise_irq(avr_io_getirq(avr, AVR_IOCTL_UART_GETIRQ('0'), UART_IRQ_INPUT), buffer[buffer_idx]);
-			buffer_idx++;
-		}
 		int state = avr_run(avr);
 		if (state == cpu_Done) {
 			fputs("CPU stopped gracefully.", stderr);
